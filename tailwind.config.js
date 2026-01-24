eabbca7f45f5eda0b7dfeaf3b874e064610d38d0
nikhil8808 <nikhilkurane7337@gmail.com>
2026-01-24 17:48:24 +0530

    middleware

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+export const darkMode = 'class';
+export const content = [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+];
