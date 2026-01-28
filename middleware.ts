c973716f228aad887e3ce22b7a6bc14f1cc900b1
nikhil8808 <nikhilkurane7337@gmail.com>
2026-01-28 23:26:49 +0530

    Add webhook route to public route matcher in middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
-  '/sign-up(.*)'
+  '/sign-up(.*)',
+   '/api/webhook(.*)', // <-- add this line to allow webhooks
 ])
 
 export default clerkMiddleware(async (auth, req) => {
